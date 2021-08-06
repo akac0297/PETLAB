1ddf064da21e0a2dd2b593cbf8be2b192062cdf0
akac0297 <akac0297@uni.sydney.edu.au>
2021-08-06 16:07:35 +1000

    functions to create df for functional analysis

--- Functional analysis/ADC and DCE analysis.py
+++ Functional analysis/ADC and DCE analysis.py
@@ -27,13 +27,60 @@ def intensityAnalysis(img,tumour_seg,patient_no,timepoint):
     iqr=np.subtract(*np.percentile(tumour, [75, 25]))
     p_5 = np.percentile(tumour, 5)
         
-    featuresDict={"Patient":patient_no, "Timepoint":timepoint, "95%":p_95, "median":median, "mean":mean, "standard dev": sd,
+    featuresDict={"PATIENT_ID":patient_no, "TIMEPOINT":timepoint, "95%":p_95, "MEDIAN":median, "MEAN":mean, "STD DEV": sd,
     "IQR":iqr, "5%":p_5}
-    series = pd.Series(featuresDict)
     
-    return(series)
+    return(featuresDict)
 
-#need to create functions to run intensityAnalysis on all ADC, MPE, TTP images. Input is a list or dictionary of patient numbers and time points, and 
-#output will be a dataframe of ADC, MPE, or TTP information for all patients and time points. Can also combine these dataframes together, but will
-#need to add an additional column specifying the modality. Also note we can use the "masked pet breast" volume and SUVs to do analysis of the breast
-#containing the tumour as well
+path="/home/alicja/"
+
+def ADCanalysis(path=path):
+    patient_list=["04","05","06","07","08","09","10","12","13","14","15","16","18","19","21","23"]
+    timepoints=["1","2","3"]
+    df=pd.DataFrame(columns=["PATIENT_ID","TIMEPOINT","95% ADC","MEDIAN ADC","MEAN ADC","STD DEV ADC","IQR ADC","5% ADC"])
+    for patient in patient_list:
+        folder="PET_LAB_PROCESSED/WES_0"+patient+"/IMAGES/"
+        for timepoint in timepoints:
+            ADC_img=sitk.ReadImage(path+folder+"WES_0"+patient+"_TIMEPOINT_"+timepoint+"MRI_DWI_ADC.nii.gz")
+            seg=sitk.ReadImage(path+folder+"WES_0"+patient_no+"_TIMEPOINT_"+timepoint+"_MRI_DCE_TUMOUR.nii.gz")
+            featuresDict = intensityAnalysis(ADC_img,seg,patient,timepoint)
+            idx=timepoints.index(str(timepoint)+str(patient))
+            df.loc[idx]=[int(patient)]+[int(timepoint)]+[featuresDict.get("95% ADC")]+[featuresDict.get("MEDIAN ADC")]+[featuresDict.get("MEAN ADC")]+[featuresDict.get("STD DEV ADC")]+[featuresDict.get("IQR ADC")]+[featuresDict.get("5% ADC")]
+    return(df)
+
+def MPEanalysis(path=path):
+    patient_list=["04","05","06","07","08","09","10","12","13","14","15","16","18","19","21","23"]
+    timepoints=["1","2","3"]
+    df=pd.DataFrame(columns=["PATIENT_ID","TIMEPOINT","95% MPE","MEDIAN MPE","MEAN MPE","STD DEV MPE","IQR MPE","5% MPE"])
+    for patient in patient_list:
+        folder="PET_LAB_PROCESSED/WES_0"+patient+"/IMAGES/"
+        for timepoint in timepoints:
+            MPE_img=sitk.ReadImage(path+folder+"WES_0"+patient+"_TIMEPOINT_"+timepoint+"MRI_T1W_DCE_MPE_sub.nii.gz")
+            seg=sitk.ReadImage(path+folder+"WES_0"+patient_no+"_TIMEPOINT_"+timepoint+"_MRI_DCE_TUMOUR.nii.gz")
+            featuresDict = intensityAnalysis(MPE_img,seg,patient,timepoint)
+            idx=timepoints.index(str(timepoint)+str(patient))
+            df.loc[idx]=[int(patient)]+[int(timepoint)]+[featuresDict.get("95% MPE")]+[featuresDict.get("MEDIAN MPE")]+[featuresDict.get("MEAN MPE")]+[featuresDict.get("STD DEV MPE")]+[featuresDict.get("IQR MPE")]+[featuresDict.get("5% MPE")]
+    return(df)
+
+def TTPanalysis(path=path):
+    patient_list=["04","05","06","07","08","09","10","12","13","14","15","16","18","19","21","23"]
+    timepoints=["1","2","3"]
+    df=pd.DataFrame(columns=["PATIENT_ID","TIMEPOINT","95% TTP","MEDIAN TTP","MEAN TTP","STD DEV TTP","IQR TTP","5% TTP"])
+    for patient in patient_list:
+        folder="PET_LAB_PROCESSED/WES_0"+patient+"/IMAGES/"
+        for timepoint in timepoints:
+            TTP_img=sitk.ReadImage(path+folder+"WES_0"+patient+"_TIMEPOINT_"+timepoint+"MRI_T1W_DCE_TTP_sub.nii.gz")
+            seg=sitk.ReadImage(path+folder+"WES_0"+patient_no+"_TIMEPOINT_"+timepoint+"_MRI_DCE_TUMOUR.nii.gz")
+            featuresDict = intensityAnalysis(TTP_img,seg,patient,timepoint)
+            idx=timepoints.index(str(timepoint)+str(patient))
+            df.loc[idx]=[int(patient)]+[int(timepoint)]+[featuresDict.get("95% TTP")]+[featuresDict.get("MEDIAN TTP")]+[featuresDict.get("MEAN TTP")]+[featuresDict.get("STD DEV TTP")]+[featuresDict.get("IQR TTP")]+[featuresDict.get("5% TTP")]
+    return(df)
+
+ADC_df=ADCanalysis(path=path)
+MPE_df=MPEanalysis(path=path)
+TTP_df=TTPanalysis(path=path)
+
+DCE_df=pd.merge(MPE_df,TTP_df[["PATIENT_ID","95% TTP","MEDIAN TTP","MEAN TTP","STD DEV TTP","IQR TTP","5% TTP"]],on="PATIENT_ID",how="left")
+FunctionalAnalysisDf=pd.merge(ADC_df,DCE_df[["PATIENT_ID","95% MPE","MEDIAN MPE","MEAN MPE","STD DEV MPE","IQR MPE","5% MPE","95% TTP","MEDIAN TTP","MEAN TTP","STD DEV TTP","IQR TTP","5% TTP"]],on="PATIENT_ID",how="left")
+
+#need to combine these dataframes together: https://stackoverflow.com/questions/17978133/python-pandas-merge-only-certain-columns
